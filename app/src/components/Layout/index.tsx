fdfa6de6975501a7a23a3383ba06bcb2c74712f3
n4ze3m <n4ze3m@gmail.com>
2023-06-04 14:10:05 +0530

    new changes

---
+++ app/src/components/Layout/index.tsx
@@ -0,0 +1,107 @@
+import { Disclosure } from "@headlessui/react";
+import { BellIcon } from "@heroicons/react/24/outline";
+import { Outlet } from "react-router-dom";
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+//@ts-ignore
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function DashboardLayout() {
+  return (
+    <>
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-white shadow-sm">
+          {({}) => (
+            <>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 justify-between">
+                  <div className="flex">
+                    <div className="flex flex-shrink-0 items-center">
+                      <img
+                        className="block h-8 w-auto lg:hidden"
+                        src="https://em-content.zobj.net/thumbs/120/openmoji/338/high-voltage_26a1.png"
+                        alt="dialoqbase"
+                      />
+                      <img
+                        className="hidden h-8 w-auto lg:block"
+                        src="https://em-content.zobj.net/thumbs/120/openmoji/338/high-voltage_26a1.png"
+                        alt="dialoqbase"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Disclosure.Panel className="sm:hidden">
+                <div className="border-t hidden border-gray-200 pt-4 pb-3">
+                  <div className="flex items-center px-4">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src={user.imageUrl}
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium text-gray-800">
+                        {user.name}
+                      </div>
+                      <div className="text-sm font-medium text-gray-500">
+                        {user.email}
+                      </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="mt-3 space-y-1">
+                    {userNavigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <div className="py-10">
+          <main>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+              {/* Replace with your content */}
+              <Outlet />
+              {/* <div className="px-4 py-8 sm:px-0">
+                <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+              </div> */}
+              {/* /End replace */}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import DashboardLayout from "./components/Layout";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        element: <Root />,
+        path: "/"
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
