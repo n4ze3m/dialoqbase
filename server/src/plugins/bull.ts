c4a8c1127831cb64929b19b6268f8f0469d53767
n4ze3m <n4ze3m@gmail.com>
2023-06-08 21:36:01 +0530

    queue fixes

--- server/src/plugins/bull.ts
+++ server/src/plugins/bull.ts
@@ -1,6 +1,8 @@
 import fp from "fastify-plugin";
 import { FastifyPluginAsync } from "fastify";
+
 import * as Queue from "bull";
+import { queueHandler } from "../queue";
 
 declare module "fastify" {
   interface FastifyInstance {
@@ -10,8 +12,13 @@ declare module "fastify" {
 
 const bullPlugin: FastifyPluginAsync = fp(async (server, options) => {
   const queue = new Queue("vector", process.env.DB_REDIS_URL!, {});
+
   await queue.isReady();
+
+  queue.process(queueHandler);
+
   server.decorate("queue", queue);
+
   server.addHook("onClose", () => {
     queue.close();
   });
