7c3aa39ce2e760280e8f85510e5e70db430fbb50
n4ze3m <n4ze3m@gmail.com>
2023-06-09 11:34:48 +0530

    cohere embedding

--- server/src/queue/index.ts
+++ server/src/queue/index.ts
@@ -18,145 +18,147 @@ export const queue = new Queue("vector", process.env.DB_REDIS_URL!, {});
 export const queueHandler = async (job: Job, done: DoneCallback) => {
   const data = job.data as QSource[];
 
-  console.log("Processing queue" );
-
-  for (const source of data) {
-    try {
-      if (source.type.toLowerCase() === "website") {
-        await prisma.botSource.update({
-          where: {
-            id: source.id,
-          },
-          data: {
-            status: "PROCESSING",
-          },
-        });
-
-        const loader = new CheerioWebBaseLoader(source.content!);
-        const docs = await loader.load();
-
-        const textSplitter = new RecursiveCharacterTextSplitter({
-          chunkSize: 1000,
-          chunkOverlap: 200,
-        });
-        const chunks = await textSplitter.splitDocuments(docs);
-
-        await DialoqbaseVectorStore.fromDocuments(
-          chunks,
-          embeddings(source.embedding),
-          {
-            botId: source.botId,
-            sourceId: source.id,
-          },
-        );
-
-        await prisma.botSource.update({
-          where: {
-            id: source.id,
-          },
-          data: {
-            status: "FINISHED",
-            isPending: false,
-          },
-        });
-      } else if (source.type.toLowerCase() === "text") {
-        await prisma.botSource.update({
-          where: {
-            id: source.id,
-          },
-          data: {
-            status: "PROCESSING",
-          },
-        });
-
-        const textSplitter = new RecursiveCharacterTextSplitter({
-          chunkSize: 1000,
-          chunkOverlap: 200,
-        });
-        const chunks = await textSplitter.splitDocuments([
-          {
-            pageContent: source.content!,
-            metadata: {
-              source: `text-${source.id}`,
+  console.log("Processing queue");
+  try {
+    for (const source of data) {
+      try {
+        if (source.type.toLowerCase() === "website") {
+          await prisma.botSource.update({
+            where: {
+              id: source.id,
             },
-          },
-        ]);
+            data: {
+              status: "PROCESSING",
+            },
+          });
 
-        await DialoqbaseVectorStore.fromDocuments(
-          chunks,
-          embeddings(source.embedding),
-          {
-            botId: source.botId,
-            sourceId: source.id,
-          },
-        );
+          const loader = new CheerioWebBaseLoader(source.content!);
+          const docs = await loader.load();
+
+          const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200,
+          });
+          const chunks = await textSplitter.splitDocuments(docs);
+
+          await DialoqbaseVectorStore.fromDocuments(
+            chunks,
+            embeddings(source.embedding),
+            {
+              botId: source.botId,
+              sourceId: source.id,
+            },
+          );
+
+          await prisma.botSource.update({
+            where: {
+              id: source.id,
+            },
+            data: {
+              status: "FINISHED",
+              isPending: false,
+            },
+          });
+        } else if (source.type.toLowerCase() === "text") {
+          await prisma.botSource.update({
+            where: {
+              id: source.id,
+            },
+            data: {
+              status: "PROCESSING",
+            },
+          });
+
+          const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200,
+          });
+          const chunks = await textSplitter.splitDocuments([
+            {
+              pageContent: source.content!,
+              metadata: {
+                source: `text-${source.id}`,
+              },
+            },
+          ]);
+
+          await DialoqbaseVectorStore.fromDocuments(
+            chunks,
+            embeddings(source.embedding),
+            {
+              botId: source.botId,
+              sourceId: source.id,
+            },
+          );
+
+          await prisma.botSource.update({
+            where: {
+              id: source.id,
+            },
+            data: {
+              status: "FINISHED",
+              isPending: false,
+            },
+          });
+        } else if (source.type.toLowerCase() === "pdf") {
+          console.log("loading pdf");
+          await prisma.botSource.update({
+            where: {
+              id: source.id,
+            },
+            data: {
+              status: "PROCESSING",
+            },
+          });
+
+          const location = source.location!;
+          const loader = new PDFLoader(location);
+          const docs = await loader.load();
+
+          const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200,
+          });
+          const chunks = await textSplitter.splitDocuments(docs);
+
+          await DialoqbaseVectorStore.fromDocuments(
+            chunks,
+            embeddings(source.embedding),
+            {
+              botId: source.botId,
+              sourceId: source.id,
+            },
+          );
+
+          await prisma.botSource.update({
+            where: {
+              id: source.id,
+            },
+            data: {
+              status: "FINISHED",
+              isPending: false,
+            },
+          });
+        }
+      } catch (e) {
+        console.log(e);
 
         await prisma.botSource.update({
           where: {
             id: source.id,
           },
           data: {
-            status: "FINISHED",
-            isPending: false,
-          },
-        });
-      } else if (source.type.toLowerCase() === "pdf") {
-        console.log("loading pdf");
-        await prisma.botSource.update({
-          where: {
-            id: source.id,
-          },
-          data: {
-            status: "PROCESSING",
-          },
-        });
-
-        const location = source.location!;
-        const loader = new PDFLoader(location);
-        const docs = await loader.load();
-
-        const textSplitter = new RecursiveCharacterTextSplitter({
-          chunkSize: 1000,
-          chunkOverlap: 200,
-        });
-        const chunks = await textSplitter.splitDocuments(docs);
-
-        await DialoqbaseVectorStore.fromDocuments(
-          chunks,
-          embeddings(source.embedding),
-          {
-            botId: source.botId,
-            sourceId: source.id,
-          },
-        );
-
-        await prisma.botSource.update({
-          where: {
-            id: source.id,
-          },
-          data: {
-            status: "FINISHED",
+            status: "FAILED",
             isPending: false,
           },
         });
       }
-    } catch (e) {
-      console.log(e);
-
-      await prisma.botSource.update({
-        where: {
-          id: source.id,
-        },
-        data: {
-          status: "FAILED",
-          isPending: false,
-        },
-      });
     }
+  } catch (e) {
+    console.log(e);
+  } finally {
+    done();
   }
-
-  done();
 };
 
 queue.process(queueHandler);
-
--- server/src/utils/embeddings.ts
+++ server/src/utils/embeddings.ts
@@ -1,13 +1,15 @@
 import "@tensorflow/tfjs-backend-cpu";
 import { TensorFlowEmbeddings } from "langchain/embeddings/tensorflow";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-
+import {CohereEmbeddings} from  'langchain/embeddings/cohere'
 export const embeddings = (embeddingsType: string) => {
   switch (embeddingsType) {
     case "tensorflow":
       return new TensorFlowEmbeddings();
     case "openai":
       return new OpenAIEmbeddings();
+    case "cohere":
+      return new CohereEmbeddings();
     default:
       return new OpenAIEmbeddings();
   }
