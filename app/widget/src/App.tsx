aa32165d538b6c626bbd2ba4c6f3c1390d383a73
n4ze3m <n4ze3m@gmail.com>
2023-07-07 22:25:46 +0530

    widget updated

--- app/widget/src/App.tsx
+++ app/widget/src/App.tsx
@@ -189,7 +189,90 @@ function App() {
                                     </code>
                                   );
                                 },
+                                a({ node, ...props }) {
+                                  return (
+                                    <a
+                                      target="_blank"
+                                      rel="noreferrer"
+                                      className="text-blue-500 hover:underline"
+                                      {...props}
+                                    >
+                                      {props.children}
+                                    </a>
+                                  );
+                                },
+                                li({ node, ...props }) {
+                                  return (
+                                    <li className="list-decimal marker:text-gray-500" {...props}>
+                                      {props.children}
+                                    </li>
+                                  );
+                                },
+                                ul({ node, ...props }) {
+                                  return (
+                                    <ul className="mx-2" {...props}>
+                                      {props.children}
+                                    </ul>
+                                  );
+                                },
+                                ol({ node, ...props }) {
+                                  return (
+                                    <ul className="mx-2" {...props}>
+                                      {props.children}
+                                    </ul>
+                                  );
+                                },
+                                h1({ node, ...props }) {
+                                  return (
+                                    <h1 className="text-2xl font-bold" {...props}>
+                                      {props.children}
+                                    </h1>
+                                  );
+                                },
+                                h2({ node, ...props }) {
+                                  return (
+                                    <h2 className="text-xl font-bold" {...props}>
+                                      {props.children}
+                                    </h2>
+                                  );
+                                },
+                                h3({ node, ...props }) {
+                                  return (
+                                    <h3 className="text-lg font-bold" {...props}>
+                                      {props.children}
+                                    </h3>
+                                  );
+                                },
+                                h4({ node, ...props }) {
+                                  return (
+                                    <h4 className="text-base font-bold" {...props}>
+                                      {props.children}
+                                    </h4>
+                                  );
+                                },
+                                h5({ node, ...props }) {
+                                  return (
+                                    <h5 className="text-sm font-bold" {...props}>
+                                      {props.children}
+                                    </h5>
+                                  );
+                                },
+                                h6({ node, ...props }) {
+                                  return (
+                                    <h6 className="text-xs font-bold" {...props}>
+                                      {props.children}
+                                    </h6>
+                                  );
+                                },
+                                p({ node, ...props }) {
+                                  return (
+                                    <p className="text-sm" {...props}>
+                                      {props.children}
+                                    </p>
+                                  );
+                                }
                               }}
+                              
                             >
                               {message.message}
                             </ReactMarkdown>
