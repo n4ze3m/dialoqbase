dcdcd28941d6ad9cdc1427c4130d85a14e8ba9c8
n4ze3m <n4ze3m@gmail.com>
2023-07-28 11:39:18 +0530

    few testing

--- server/src/utils/models.ts
+++ server/src/utils/models.ts
@@ -1,6 +1,7 @@
 import { ChatAnthropic } from "langchain/chat_models/anthropic";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import {ChatGooglePaLM  } from "langchain/chat_models/googlepalm";
+import { ChatGooglePaLM } from "langchain/chat_models/googlepalm";
+import { HuggingFaceInference } from "langchain/llms/hf";
 
 export const chatModelProvider = (
   provider: string,
@@ -30,6 +31,13 @@ export const chatModelProvider = (
         apiKey: process.env.GOOGLE_API_KEY,
         ...otherFields,
       });
+    case "huggingface-api":
+      console.log("using huggingface-api");
+      return new HuggingFaceInference({
+        modelName: huggingfaceModels[modelName],
+        temperature: temperature,
+        ...otherFields,
+      });
     default:
       console.log("using default");
       return new ChatOpenAI({
@@ -38,6 +46,12 @@ export const chatModelProvider = (
         ...otherFields,
       });
   }
+};
+
+export const huggingfaceModels: {
+  [key: string]: string;
+} = {
+  "falcon-7b-instruct-inference": "tiiuae/falcon-7b-instruct",
 };
 
 export const streamingSupportedModels = [
@@ -53,3 +67,15 @@ export const streamingSupportedModels = [
 export const isStreamingSupported = (model: string) => {
   return streamingSupportedModels.includes(model);
 };
+
+export const supportedModels = [
+  "gpt-3.5-turbo",
+  "gpt-3.5-turbo-16k",
+  "gpt-4-0613",
+  "gpt-4",
+  "claude-1",
+  "claude-2",
+  "claude-instant-1",
+  "google-bison",
+  "falcon-7b-instruct-inference"
+];
