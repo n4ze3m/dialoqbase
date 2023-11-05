f04173f7f66d1bec2ba4f6efc1440c667ad90506
n4ze3m <n4ze3m@gmail.com>
2023-11-05 20:35:14 +0530

    Add cache directory for production environment in pipelineTransformer function

--- server/src/utils/pipleline.js
+++ server/src/utils/pipleline.js
@@ -1,6 +1,9 @@
 exports.piplelineTransformer = async () => {
-    let { pipeline,env } = await import("@xenova/transformers");
-    env.useBrowserCache = false;
-    env.allowLocalModels = false;
-    return pipeline;
-}
+  let { pipeline, env } = await import("@xenova/transformers");
+  env.useBrowserCache = false;
+  env.allowLocalModels = false;
+  if (process.env.NODE_ENV === "production") {
+    env.cacheDir = "./.cache";
+  }
+  return pipeline;
+};
