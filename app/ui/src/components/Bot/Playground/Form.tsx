32bbbd44caab14819d2e19feb7f46bcfb4e8c64f
n4ze3m <n4ze3m@gmail.com>
2023-11-18 18:59:26 +0530

    Fix SVG icon alignment

--- app/ui/src/components/Bot/Playground/Form.tsx
+++ app/ui/src/components/Bot/Playground/Form.tsx
@@ -155,20 +155,20 @@ export const PlaygroundgForm = () => {
                       "animate-pulse ring-2 ring-blue-500 rounded-full ring-opacity-50"
                     }`}
                   >
-                   <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      className="h-6 w-6"
-      viewBox="0 0 24 24"
-    >
-      <path d="M12 2a3 3 0 00-3 3v7a3 3 0 006 0V5a3 3 0 00-3-3z"></path>
-      <path d="M19 10v2a7 7 0 01-14 0v-2"></path>
-      <path d="M12 19L12 22"></path>
-    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-6 w-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2a3 3 0 00-3 3v7a3 3 0 006 0V5a3 3 0 00-3-3z"></path>
+                      <path d="M19 10v2a7 7 0 01-14 0v-2"></path>
+                      <path d="M12 19L12 22"></path>
+                    </svg>
                   </button>
                 </div>
               )}
