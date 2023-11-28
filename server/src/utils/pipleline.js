045e1e0f163c9dfa73fa5b216f9f84e2f2a9c69a
n4ze3m <n4ze3m@gmail.com>
2023-11-28 23:40:19 +0530

    allow local models

--- server/src/utils/pipleline.js
+++ server/src/utils/pipleline.js
@@ -1,9 +1,9 @@
 exports.piplelineTransformer = async () => {
   let { pipeline, env } = await import("@xenova/transformers");
   env.useBrowserCache = false;
-  env.allowLocalModels = false;
   if (process.env.NODE_ENV === "production") {
-    env.cacheDir = "./.cache";
+    
+    env.cacheDir = "./uploads/.cache";
   }
   return pipeline;
 };
