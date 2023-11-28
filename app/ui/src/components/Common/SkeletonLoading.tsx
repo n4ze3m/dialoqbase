39abce59db732d5572719a0743519a8ab1a4ae68
n4ze3m <n4ze3m@gmail.com>
2023-11-28 22:53:48 +0530

    Update package versions and fix loading skeleton styles

--- app/ui/src/components/Common/SkeletonLoading.tsx
+++ app/ui/src/components/Common/SkeletonLoading.tsx
@@ -1,10 +1,10 @@
 import { Skeleton } from "antd";
 
-export const SkeletonLoading = () => {
+export const SkeletonLoading = ({ className = "mt-6" }: { className?: string }) => {
   return (
-    <>
+    <div className={className}>
       <Skeleton active />
       <Skeleton active className="mt-3" />
-    </>
+    </div>
   );
 };
