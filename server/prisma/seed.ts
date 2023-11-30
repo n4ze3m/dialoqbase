9a6c3fd8ab9d46a8e59e39bd04f2c0a27a630480
n4ze3m <n4ze3m@gmail.com>
2023-12-01 00:08:37 +0530

    Add new models to seed.ts

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const MODELS = [
+const MODELS: {
+  name: string;
+  model_id: string;
+  model_type: string;
+  model_provider?: string;
+  stream_available?: boolean;
+  local_model?: boolean;
+  config?: string;
+}[] = [
   {
     name: "Claude 2.1 (Anthropic)",
     model_id: "claude-2.1-dbase",
@@ -17,6 +25,24 @@ const MODELS = [
     model_type: "chat",
     model_provider: "Anthropic",
     stream_available: true,
+    local_model: false,
+    config: "{}",
+  },
+  {
+    name: "Yi 34B 200k (Fireworks)",
+    model_id: "accounts/fireworks/models/yi-34b-200k",
+    model_type: "instruct",
+    stream_available: true,
+    model_provider: "Fireworks",
+    local_model: false,
+    config: "{}",
+  },
+  {
+    model_id: "accounts/fireworks/models/yi-34b-200k-capybara",
+    name: "Capybara 34B (Fireworks)",
+    model_type: "chat",
+    stream_available: true,
+    model_provider: "Fireworks",
     local_model: false,
     config: "{}",
   },
