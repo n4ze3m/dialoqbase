a05bbb02bda6a71c06f223fdcd9e139230c03e82
n4ze3m <n4ze3m@gmail.com>
2024-04-22 10:47:52 +0530

    Add Ability to configure default chat and embedding model

--- app/ui/src/routes/settings/application.tsx
+++ app/ui/src/routes/settings/application.tsx
@@ -1,16 +1,19 @@
-import { Form, InputNumber, Switch, notification } from "antd";
+import { Form, InputNumber, Switch, notification, Select } from "antd";
 import React from "react";
 import api from "../../services/api";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { SettingsLayout } from "../../Layout/SettingsLayout";
 import { SkeletonLoading } from "../../components/Common/SkeletonLoading";
 import { useNavigate } from "react-router-dom";
 import { ApplicationCard } from "../../components/Settings/Application/ApplicationCard";
+import { useCreateConfig } from "../../hooks/useCreateConfig";
 
 export default function SettingsApplicationRoot() {
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const { data: models, status: modeStatus } = useCreateConfig();
+  const queryClient = useQueryClient();
 
   const { data, status } = useQuery(["fetchApplicationSettings"], async () => {
     const response = await api.get("/admin/dialoqbase-settings");
@@ -20,6 +23,8 @@ export default function SettingsApplicationRoot() {
       allowUserToRegister: boolean;
       defaultChunkSize: number;
       defaultChunkOverlap: number;
+      defaultChatModel: string;
+      defaultEmbeddingModel: string;
     };
   });
 
@@ -43,6 +48,7 @@ export default function SettingsApplicationRoot() {
     onUpdateApplicatoon,
     {
       onSuccess: (data) => {
+        queryClient.invalidateQueries(["fetchBotCreateConfig"]);
         notification.success({
           message: "Success",
           description: data.message,
@@ -128,6 +134,66 @@ export default function SettingsApplicationRoot() {
                     valuePropName="checked"
                   >
                     <Switch />
+                  </Form.Item>
+                  <Form.Item
+                    label="Default Chat Model"
+                    name="defaultChatModel"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please select default chat model!",
+                      },
+                    ]}
+                  >
+                    <Select
+                      showSearch
+                      filterOption={(input, option) =>
+                        (option?.label?.toLowerCase() ?? "").includes(
+                          input?.toLowerCase()
+                        ) ||
+                        (option?.value?.toLowerCase() ?? "").includes(
+                          input?.toLowerCase()
+                        )
+                      }
+                      filterSort={(optionA, optionB) =>
+                        (optionA?.label ?? "")
+                          .toLowerCase()
+                          .localeCompare((optionB?.label ?? "").toLowerCase())
+                      }
+                      placeholder="Select a Chat Model"
+                      options={models?.chatModel || []}
+                      loading={modeStatus === "loading"}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label="Default Embedding Model"
+                    name="defaultEmbeddingModel"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please select default embedding model!",
+                      },
+                    ]}
+                  >
+                    <Select
+                      showSearch
+                      filterOption={(input, option) =>
+                        (option?.label?.toLowerCase() ?? "").includes(
+                          input?.toLowerCase()
+                        ) ||
+                        (option?.value?.toLowerCase() ?? "").includes(
+                          input?.toLowerCase()
+                        )
+                      }
+                      filterSort={(optionA, optionB) =>
+                        (optionA?.label ?? "")
+                          .toLowerCase()
+                          .localeCompare((optionB?.label ?? "").toLowerCase())
+                      }
+                      placeholder="Select a Embedding Model"
+                      options={models?.embeddingModel || []}
+                      loading={modeStatus === "loading"}
+                    />
                   </Form.Item>
                 </div>
                 <div className="bg-gray-50 border-x border-b rounded-b-md rounded-x-md px-4 py-3 text-right sm:px-6 dark:bg-[#141414] dark:border-gray-600">
