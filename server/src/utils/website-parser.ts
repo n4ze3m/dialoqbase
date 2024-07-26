26b81a198d3c1b0471e84c087c997c8b0a232696
n4ze3m <n4ze3m@gmail.com>
2024-07-26 22:43:18 +0530

    chore: Update npm dependencies and website parser

--- server/src/utils/website-parser.ts
+++ server/src/utils/website-parser.ts
@@ -1,10 +1,47 @@
-import * as cheerio from "cheerio";
-import TurndownService from "turndown";
-let turndownService = new TurndownService();
+import * as cheerio from "cheerio"
+import TurndownService from "turndown"
+import { Readability, isProbablyReaderable } from "@mozilla/readability"
+import { JSDOM } from "jsdom"
 
 export const websiteParser = (html: string) => {
-  const $ = cheerio.load(html);
-  const mainContent = $('[role="main"]').html() || $("main").html() || $.html();
-  const markdown = turndownService.turndown(mainContent);
-  return markdown;
+  const $ = cheerio.load(html)
+
+  $('script, style, link, svg, [src^="data:image/"]').remove()
+  
+  const jsdom = new JSDOM($.html())
+
+  const doc = jsdom.window.document
+
+  if (isProbablyReaderable(doc)) {
+    const reader = new Readability(doc)
+    const article = reader.parse()
+    const turndownService = new TurndownService({
+      headingStyle: 'atx',
+      codeBlockStyle: 'fenced'
+    })
+    return turndownService.turndown(article.content).trim()
+  }
+
+
+
+  $('*').each((_, element) => {
+    if ('attribs' in element) {
+      const attributes = element.attribs
+      for (const attr in attributes) {
+        if (attr !== 'href' && attr !== 'src') {
+          $(element).removeAttr(attr)
+        }
+      }
+    }
+  })
+
+  const mainContent = $('[role="main"]').html() || $("main").html() || $("body").html() || ""
+
+  const turndownService = new TurndownService({
+    headingStyle: 'atx',
+    codeBlockStyle: 'fenced'
+  })
+  const markdown = turndownService.turndown(mainContent)
+
+  return markdown.trim()
 };
